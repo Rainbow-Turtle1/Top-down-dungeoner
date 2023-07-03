0fa1e48379f1a15447f84de2fbcb49c71d4333fd
Rainbow-Turtle1 <72671611+Rainbow-Turtle1@users.noreply.github.com>
2023-07-03 14:28:32 +0100

    beginning of adding inventory

---
+++ Top down dungeoner (game)/Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int inventoryHeight;
+    public int inventoryWidth;
+    public InventorySlot[,] inventorySlots;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventorySlots = new InventorySlot[inventoryWidth, inventoryHeight];
+        
+    } 
+
+}
